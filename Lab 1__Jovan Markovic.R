8699872ac92bfea35c39f726ec7f1612f6c5ee6f
Jovanmarkovic323 <jmarkovic@umass.edu>
2020-08-31 15:34:19 +0000

    Uploading Lab 1 files

---
+++ Lab 1__Jovan Markovic.R
@@ -0,0 +1,24 @@
+
+
+DNA <- "ATCGGGA"
+### Exercise 1
+x <- 3
+y <- 15
+x + y
+x - y
+x * y
+x / y
+
+### Exercise 2
+x <- 2
+y <- 3
+x^5 > y^4
+
+### Exercise 3
+Q <- c(22,62,148,43,129)
+sum (Q)
+Q / 404
+
+### Exercise 4
+nucleotides <- c("A", "T", "C", "G")
+sort(nucleotides)
